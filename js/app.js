8eb2ffae4e8fa74372b6573601c0493b88861e58
LaithAlOudat <aloudat.laith@gmail.com>
2021-03-29 00:04:10 +0300

    add new stuff

--- js/app.js
+++ js/app.js
@@ -1,13 +1,24 @@
 'use strict';
-
-// globe variables and functions:
-let productsImages = ['bag.jpg', 'banana.jpg', 'bathroom.jpg', 'boots.jpg', 'breakfast.jpg', 'bubblegum.jpg', 'chair.jpg', 'cthulhu.jpg', 'dog-duck.jpg', 'dragon.jpg', 'pen.jpg', 'pet-sweep.jpg', 'scissors.jpg', 'shark.jpg', 'sweep.png', 'tauntaun.jpg', 'unicorn.jpg', 'usb.gif', 'water-can.jpg', 'wine-glass.jpg'];
+// 'sweep.png', 'usb.gif',
+// globe variables and functions:-
+let productsImages = ['bag.jpg', 'banana.jpg', 'bathroom.jpg', 'boots.jpg', 'breakfast.jpg',
+  'bubblegum.jpg', 'chair.jpg', 'cthulhu.jpg', 'dog-duck.jpg', 'dragon.jpg', 'pen.jpg', 'pet-sweep.jpg',
+  'scissors.jpg', 'shark.jpg', 'tauntaun.jpg', 'unicorn.jpg', 'water-can.jpg', 'wine-glass.jpg'];
 let numOfRound = 0;
+let leftIndex;
+let middleIndex;
+let rightIndex;
+// get images section and it's element by ID:
 let imgsSection = document.getElementById('imgsSection');
 let leftImage = document.getElementById('leftImage');
 let middleImage = document.getElementById('middleImage');
-let rightImage = document.getElementById('rightIMage');
+let rightImage = document.getElementById('rightImage');
+// button section and button element by ID:
+let btnSection = document.getElementById('btnSection');
+let resultBtn = document.getElementById('resultBtn');
 
+
+// console.log(productsImages[17]);
 
 // generate a random number:
 function getRandomNumber(max, min) {
@@ -15,58 +26,43 @@ function getRandomNumber(max, min) {
 }
 
 // constructor to select images
-function Products(nameOfProduct) {
+function Product(nameOfProduct) {
   this.nameOfProduct = nameOfProduct;
-  this.image = `./imgs/${nameOfProduct}.jpg/`;
+  this.image = `./imgs/${nameOfProduct}.jpg`;
   this.shownTime = 0;
   this.voteTime = 0;
-  Products.all.push(this);
+  Product.all.push(this);
 }
-Products.all = [];
+Product.all = [];
+
+// creating instances for all products' images array:
 
 for (let i = 0; i < productsImages.length; i++) {
-  new Products(productsImages[i]);
-}
-
-
-// adding an event listener and function for the clicking event:
-
-imgsSection.addEventListener('click', mouseclicking);
-
-function mouseclicking(event) {
-  if (event.target.id === leftImage.id) {
-    Products.all[leftImage].voteTime++;
-  } if (event.target.id === middleImage.id) {
-    Products.all[middleImage].voteTime++;
-  } else {
-    Products.all[rightImage].voteTime++;
-  }
+  new Product(productsImages[i]);
 }
 
 // function for render the products' images:
 function render() {
-  leftImage = getRandomNumber(0, Products.all.length - 1);
-  leftImage.src = Products.all[leftImage].path;
-  leftImage.alt = Products.all[leftImage].nameOfProduct;
-  leftImage.title = Products.all[leftImage].nameOfProduct;
+  leftIndex = getRandomNumber(0, Product.all.length - 1);
+  // leftImage.src = Product.all[leftIndex].path;
+  leftImage.title = Product.all[leftIndex].nameOfProduct;
+  leftImage.alt = Product.all[leftIndex].nameOfProduct;
+  middleIndex = getRandomNumber(0, Product.all.length - 1);
+  // middleImage.src = Product.all[middleIndex].path;
+  middleImage.title = Product.all[middleIndex].nameOfProduct;
+  middleImage.alt = Product.all[middleIndex].nameOfProduct;
+  rightIndex = getRandomNumber(0, Product.all.length - 1);
+  // rightImage.src = Product.all[rightIndex].path;
+  rightImage.title = Product.all[rightIndex].nameOfProduct;
+  rightImage.alt = Product.all[rightIndex].nameOfProduct;
 
-  middleImage = getRandomNumber(0, Products.all.length - 1);
-  middleImage.src = Products.all[middleImage].path;
-  middleImage.alt = Products.all[middleImage].nameOfProduct;
-  middleImage.title = Products.all[middleImage].nameOfProduct;
-
-  rightImage = getRandomNumber(0, Products.all.length - 1);
-  rightImage.src = Products.all[rightImage].path;
-  rightImage.alt = Products.all[rightImage].nameOfProduct;
-  rightImage.title = Products.all[rightImage].nameOfProduct;
-
-  if (leftImage.src === middleImage.src) {
+  if (leftIndex === middleIndex) {
     render();
   } else {
-    if (leftImage.src === rightImage.src) {
+    if (leftIndex === rightIndex) {
       render();
     } else {
-      if (middleImage.src === rightImage.scr) {
+      if (middleIndex === rightIndex) {
         render();
       }
     }
@@ -75,3 +71,33 @@ function render() {
 }
 
 render();
+
+
+// adding an event listener and function to vote++ and clicking event:
+
+imgsSection.addEventListener('click', mouseclicking);
+
+function mouseclicking(event) {
+
+  for (let i = 0; i < 25; i++) {
+    if (event.target.id === 'leftImage') {
+      Product.all[leftIndex].voteTime++;
+      numOfRound += numOfRound;
+    } if (event.target.id === 'middleImage') {
+      Product.all[middleIndex].voteTime++;
+      numOfRound += numOfRound;
+    } else {
+      if (event.target.id === 'rightImage') {
+        Product.all[rightIndex].voteTime++;
+        numOfRound += numOfRound;
+      } else {
+        event.target.id === 'hola';
+        break;
+      }
+
+    }
+  }
+  console.log(numOfRound);
+  console.log(Product.all.voteTime);
+  console.log(Product.all.shownTime);
+}
