52828ec9479d0a40bd3f63e86b1f5ea97056517d
LaithAlOudat <aloudat.laith@gmail.com>
2021-03-29 22:17:02 +0300

    modify1

--- js/app.js
+++ js/app.js
@@ -1,13 +1,14 @@
 'use strict';
-// 'sweep.png', 'usb.gif',
 // globe variables and functions:-
 let productsImages = ['bag.jpg', 'banana.jpg', 'bathroom.jpg', 'boots.jpg', 'breakfast.jpg',
   'bubblegum.jpg', 'chair.jpg', 'cthulhu.jpg', 'dog-duck.jpg', 'dragon.jpg', 'pen.jpg', 'pet-sweep.jpg',
   'scissors.jpg', 'shark.jpg', 'tauntaun.jpg', 'unicorn.jpg', 'water-can.jpg', 'wine-glass.jpg'];
-let numOfRound = 0;
+let rounds = 25;
+let numOfRounds = 0;
 let leftIndex;
 let middleIndex;
 let rightIndex;
+
 // get images section and it's element by ID:
 let imgsSection = document.getElementById('imgsSection');
 let leftImage = document.getElementById('leftImage');
@@ -18,17 +19,16 @@ let btnSection = document.getElementById('btnSection');
 let resultBtn = document.getElementById('resultBtn');
 
 
-// console.log(productsImages[17]);
 
 // generate a random number:
 function getRandomNumber(max, min) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-// constructor to select images
+// constructor for the products' images:
 function Product(nameOfProduct) {
   this.nameOfProduct = nameOfProduct;
-  this.image = `./imgs/${nameOfProduct}.jpg`;
+  this.image = `/../imgs/${nameOfProduct}`;
   this.shownTime = 0;
   this.voteTime = 0;
   Product.all.push(this);
@@ -44,15 +44,16 @@ for (let i = 0; i < productsImages.length; i++) {
 // function for render the products' images:
 function render() {
   leftIndex = getRandomNumber(0, Product.all.length - 1);
-  // leftImage.src = Product.all[leftIndex].path;
+  // console.log(Product.all[leftIndex].image);
+  leftImage.src = Product.all[leftIndex].image;
   leftImage.title = Product.all[leftIndex].nameOfProduct;
   leftImage.alt = Product.all[leftIndex].nameOfProduct;
   middleIndex = getRandomNumber(0, Product.all.length - 1);
-  // middleImage.src = Product.all[middleIndex].path;
+  middleImage.src = Product.all[middleIndex].image;
   middleImage.title = Product.all[middleIndex].nameOfProduct;
   middleImage.alt = Product.all[middleIndex].nameOfProduct;
   rightIndex = getRandomNumber(0, Product.all.length - 1);
-  // rightImage.src = Product.all[rightIndex].path;
+  rightImage.src = Product.all[rightIndex].image;
   rightImage.title = Product.all[rightIndex].nameOfProduct;
   rightImage.alt = Product.all[rightIndex].nameOfProduct;
 
@@ -73,31 +74,140 @@ function render() {
 render();
 
 
-// adding an event listener and function to vote++ and clicking event:
+// 1) adding an event listener
+// 2) adding counter to count vote, shown time and number of rounds:
 
-imgsSection.addEventListener('click', mouseclicking);
-
-function mouseclicking(event) {
-
-  for (let i = 0; i < 25; i++) {
-    if (event.target.id === 'leftImage') {
-      Product.all[leftIndex].voteTime++;
-      numOfRound += numOfRound;
-    } if (event.target.id === 'middleImage') {
-      Product.all[middleIndex].voteTime++;
-      numOfRound += numOfRound;
-    } else {
-      if (event.target.id === 'rightImage') {
+imgsSection.addEventListener('click', mouseClicking);
+function mouseClicking(event) {
+  if (event.target.id !== 'imgsSection') {
+    if (numOfRounds < rounds) {
+      numOfRounds++;
+      if (event.target.id === leftImage.id) {
+        Product.all[leftIndex].voteTime++;
+      }
+      if (event.target.id === middleImage.id) {
+        Product.all[middleIndex].voteTime++;
+      }
+      if (event.target.id === rightImage.id) {
         Product.all[rightIndex].voteTime++;
-        numOfRound += numOfRound;
-      } else {
-        event.target.id === 'hola';
-        break;
+
       }
 
+      render();
+
+
     }
+
+    let ulEl = document.getElementById('listsSection');
+    let liEl;
+    for (let i = 0; i < Product.all.length; i++) {
+      Product.voteTime.push(Product.all[i].voteTime);
+      Product.shownTime.push(Product.all[i].shownTime);
+      liEl = document.createElement('li');
+      liEl.textContent = `${Product.all[i].nameOfProduct} has ${Product.all[i].shownTime} views and has ${Product.all[i].voteTime} votes.`;
+      ulEl.appendChild(liEl);
+    }
+    imgsSection.removeEventListener('click', mouseClicking);
+    // console.log('voteTime ', Product.voteTime);
+    // console.log('shownTime ', Product.shownTime);
   }
-  console.log(numOfRound);
-  console.log(Product.all.voteTime);
-  console.log(Product.all.shownTime);
 }
+
+// imgsSection.addEventListener('click', mouseclicking);
+
+// function mouseclicking(event) {
+//   // rounds++;
+//   // if (event.target.id !== 'imgsSection') {
+//   //   if (event.target.id === rightImage.id) {
+//   //     Product.all[rightIndex].voteTime++;
+//   //   } else {
+//   //     if (event.target.id === leftImage.id) {
+//   //       Product.all[leftIndex].voteTime++;
+
+//   //     }
+//   //   }
+
+//   if (event.target.id === 'leftImage') {
+//     Product.all[leftIndex].voteTime++;
+//     rounds--;
+//     render();
+//   } if (event.target.id === 'middleImage') {
+//     Product.all[middleIndex].voteTime++;
+//     rounds--;
+//     render();
+//   } else {
+//     if (event.target.id === 'rightImage') {
+//       Product.all[rightIndex].voteTime++;
+//       rounds--;
+//       render();
+
+//     }
+
+//   }
+// }
+// render();
+
+
+// console.log(rounds);
+// console.log(Product.voteTime);
+// console.log(Product.shownTime);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 'sweep.png', 'usb.gif',
